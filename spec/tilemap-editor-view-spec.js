107f9f5c7b938d82f61fb95f4472f1bc23e029d1
Jono Mingard <reason.koan@gmail.com>
2016-09-22 12:44:21 +1200

    Tilemap and tileset editor skeleton

---
+++ spec/tilemap-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TilemapEditorView from '../lib/tilemap-editor-view';
+
+describe('TilemapEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
